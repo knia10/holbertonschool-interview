169c0b6a5fadac272e1d8abe434db9a67a0e5292
knia10 <kelly.stephania@hotmail.com>
2022-04-21 18:08:34 -0500

    Add file 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
+
+A key with the same number as a box opens that box
+The first box boxes[0] is unlocked
+Return True if all boxes can be opened, else return False
+'''
+
+def canUnlockAll(boxes):
+    '''
+    Method that determines if all the boxes can be opened.
+    '''
+    unlock = [0]
+    lenbox = len(boxes)
+    for i in unlock:
+        for key in boxes[i]:
+            if key not in unlock and key < lenbox:
+                unlock.append(key)
+    if len(unlock) != len(boxes):
+        return False
+    return True
