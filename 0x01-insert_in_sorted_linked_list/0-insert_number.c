2019d74abd256fff6d95338a880a6b2bab9fcbb4
knia10 <kelly.stephania@hotmail.com>
2022-08-11 17:22:44 -0500

    change file

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,35 +1,35 @@
 #include "lists.h"
 /**
  * insert_node - function, inserts a number into a sorted singly linked list.
- * @head: head of linked list
+ * @head: double pointer to head of linked list
  * @number: number to insert
  * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *current = NULL;
 	listint_t *new_node = NULL;
+	listint_t *current_node = NULL;
 
 	if (head == NULL)
 		return (NULL);
-	current = *head;
+	current_node = *head;
 	new_node = malloc(sizeof(listint_t));
 	new_node->n = number;
 	new_node->next = NULL;
-	if (current == NULL)
+	if (current_node == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-	if (number < current->n)
+	if (number < current_node->n)
 	{
-		new_node->next = current;
+		new_node->next = current_node;
 		*head = new_node;
 		return (new_node);
 	}
-	while (current->next && current->next->n <= number)
-		current = current->next;
-	new_node->next = current->next;
-	current->next = new_node;
+	while (current_node->next && current_node->next->n <= number)
+		current_node = current_node->next;
+	new_node->next = current_node->next;
+	current_node->next = new_node;
 	return (new_node);
 }
