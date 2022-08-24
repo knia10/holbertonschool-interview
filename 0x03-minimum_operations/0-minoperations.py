c3d7b25668444608ac7e84bfd30d619231206161
knia10 <kelly.stephania@hotmail.com>
2022-08-24 12:05:10 -0500

    Add 2 new lines

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,6 +6,8 @@ Copy All and Paste. Given a number n,
 write a method that calculates the fewest
 number of operations needed to result in exactly n H characters in the file.
 '''
+
+
 def minOperations(n):
     if n == 0:
         return 0
